82ff5f303d115c2fbccf7227fe85698e803be344
Jonathan <jonathan.malagueta@gmail.com>
2021-08-18 13:46:32 -0400

    terminando primeira aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CourseListComponent } from './courses/course-list.component';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import {StarComponent} from "./star/star.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
